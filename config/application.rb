fa3cfb8f329fff6d3d566f78c3d601bca8d559e8
sanemat <o.gata.ken@gmail.com>
2015-01-09 16:49:21 +0900

    chore(rack): use rack-health at last

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,6 @@ module FishSauce
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.middleware.insert_before ActionDispatch::Static, Rack::Health
+    config.middleware.use Rack::Health
   end
 end
